32866f19daea2791d60cdaa08876bea519bf31f1
xavireina <xavierreina93@gmail.com>
2021-11-22 20:38:04 +0100

    done

---
+++ app.js
@@ -0,0 +1,20 @@
+// Require Express
+const express = require('express');
+
+
+
+// Express server handling requests and responses
+const app = express();
+app.use(express.static('public'));
+
+// our first Route:
+
+app.get('/', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+  
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+
+// Server Started
+app.listen(3000, () => console.log('My first app listening on port 3000! '));
+
